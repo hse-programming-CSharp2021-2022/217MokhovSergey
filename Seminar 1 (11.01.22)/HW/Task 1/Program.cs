bd404d28da527c2641d5d50a7400f4e759dc40d5
foxleren <foxleren@gmail.com>
2022-03-20 17:26:54 +0300

    add Homework 1

---
+++ Seminar 1 (11.01.22)/HW/Task 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        delegate int Cast(double a);
+
+        static void Main(string[] args)
+        {
+            Cast cast1 = delegate (double a)
+            {
+                return (int)a % 2 == 0 ? (int)a : (int)a + 1;
+            };
+
+            Cast cast2 = delegate (double a)
+            {
+                return ((int)a).ToString().Length;
+            };
+
+            Console.WriteLine("Тесты для cast1 и cast2:");
+            Console.WriteLine(cast1(1.55));
+            Console.WriteLine(cast1(99.55));
+            Console.WriteLine(cast2(1.55));
+            Console.WriteLine(cast2(99.55));
+
+            Cast cast3 = cast1 + cast2;
+
+            Console.WriteLine("Тесты для cast3:");
+            Console.WriteLine(cast3(1.55));
+            Console.WriteLine(cast3(99.55));
+
+            Cast LambdaCast1 = (double a) => (int)a % 2 == 0 ? (int)a : (int)a + 1;
+
+            Cast LambdaCast2 = (double a) => ((int)a).ToString().Length;
+
+            cast3 -= cast2;
+
+            Console.WriteLine("Тесты для cast3:");
+            Console.WriteLine(cast3(1.55));
+            Console.WriteLine(cast3(99.55));
+        }
+    }
+}
+
---
+++ Seminar 1 (11.01.22)/HW/Task 3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        delegate double DelegateConvertTemperature(double t);
+
+        class TemperatureConvertImp
+        {
+            public double CelsiusToFahrenheit(double t) => (double)5 / 9 * (t - 32);
+
+            public double FahrenheitToCelsius(double t) => (double)9 / 5 * t + 32;
+        }
+
+        class StaticTempConverts
+        {
+            public static double CtoK(double temp) => temp + 273.15;
+            public static double KtoC(double temp) => temp - 273.15;
+
+            public static double CtoR(double temp) => (temp + 273.15) * (double)9 / 5;
+            public static double RtoC(double temp) => (temp - 491.67) * (double)5 / 9;
+
+            public static double CtoRe(double temp) => temp * (double)4 / 5;
+            public static double RetoC(double temp) => temp * (double)5 / 4;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            TemperatureConvertImp TempClass = new();
+
+            DelegateConvertTemperature CelToFar = TempClass.CelsiusToFahrenheit;
+            DelegateConvertTemperature FarToCel = TempClass.FahrenheitToCelsius;
+
+            Console.WriteLine(CelToFar(100));
+            Console.WriteLine(FarToCel(50));
+
+            List<DelegateConvertTemperature> list = new()
+            {
+                CelToFar,
+                FarToCel,
+                StaticTempConverts.CtoK,
+                StaticTempConverts.CtoR,
+                StaticTempConverts.CtoRe
+            };
+
+            Console.WriteLine("Введите значение в Цельсиях:");
+            int input = 0;
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Введите значение в Цельсиях:");
+
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Method.Name}: {item(input)}");
+            }
+
+        }
+    }
+}

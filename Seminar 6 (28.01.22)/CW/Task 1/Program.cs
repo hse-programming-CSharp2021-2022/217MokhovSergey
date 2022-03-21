639de97d75b7ec15ae5ba478d51c042112e1fbfc
foxleren <foxleren@gmail.com>
2022-03-21 14:47:17 +0300

    add class work 6

---
+++ Seminar 6 (28.01.22)/CW/Task 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleComp triangle = new(new Point(0, 0), new Point(0, 1), new Point(1, 0));
+            Console.WriteLine(triangle.Area + " " + triangle.IsInside(new Point(0.5, 1)));
+        }
+    }
+    class Point
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public Point(double x0, double y0)
+        {
+            x = x0;
+            y = y0;
+        }
+        public double FindDistance(Point A)
+        {
+            return Math.Sqrt(Math.Pow(x - A.x, 2) + Math.Pow(y - A.y, 2));
+        }
+    }
+    class TriangleComp
+    {
+        private Point A1, A2, A3;
+        private double AB { get; set; }
+        private double BC { get; set; }
+        private double CA { get; set; }
+        public double Area
+        {
+            get
+            {
+                double p = (AB + BC + CA) / 2;
+                return Math.Sqrt(p * (p - AB) * (p - BC) * (p - CA));
+            }
+        }
+        public bool IsInside(Point P)
+        {
+            double a = (A1.x - P.x) * (A2.y - A1.x) - (A2.x - A1.x) * (A1.y - P.y);
+            double b = (A2.x - P.x) * (A3.x - A2.x) - (A3.x - A2.x) * (A2.y - P.y);
+            double c = (A1.x - P.x) * (A1.y - A3.y) - (A1.x - A3.x) * (A3.y - P.y);
+
+            return (a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0);
+        }
+        public TriangleComp(Point A, Point B, Point C)
+        {
+            A1 = A;
+            A2 = B;
+            A3 = C;
+            AB = A.FindDistance(B);
+            CA = A.FindDistance(C);
+            BC = B.FindDistance(C);
+        }
+    }
+}
---
+++ Seminar 6 (28.01.22)/CW/Task 2/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    interface IJump
+    {
+        void Jump();
+    }
+
+    interface IRun
+    {
+        void Run();
+    }
+
+    abstract class Animal : IComparable<Animal>
+    {
+        public int Age
+        {
+            get;
+            protected set;
+        }
+        public int CompareTo(Animal animal)
+        {
+            if (Age < animal.Age)
+            {
+                return 1;
+            }
+
+            if (Age > animal.Age)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    class Cockroach : Animal, IRun
+    {
+        public int Speed { get; set; }
+        public Cockroach(int age, int speed)
+        {
+            Speed = speed;
+            Age = age;
+        }
+        public void Run()
+        {
+            Console.WriteLine("Таракан бегает со скоростью {0} км/ч", Speed);
+        }
+    }
+
+    class CockroachComparer : IComparer<Cockroach>
+    {
+        public int Compare(Cockroach a, Cockroach b)
+        {
+            if (a.Speed < b.Speed)
+            {
+                return 1;
+            }
+
+            if (a.Speed > b.Speed)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+    class Kangaroo : Animal, IJump
+    {
+        public int Length { get; }
+
+        public Kangaroo(int age, int length)
+        {
+            Length = length;
+            Age = age;
+        }
+        public void Jump()
+        {
+            Console.WriteLine("Кенгуру прыгает на {0} м", Length);
+        }
+
+    }
+    class KangarooComparer : IComparer<Kangaroo>
+    {
+        public int Compare(Kangaroo a, Kangaroo b)
+        {
+            if (a.Length < b.Length)
+            {
+                return 1;
+            }
+
+            if (a.Length > b.Length)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+    class Cheetah : Animal, IJump, IRun
+    {
+        public int Length { get; }
+        public int Speed { get; }
+
+        public Cheetah(int age, int speed, int length)
+        {
+            Length = length;
+            Age = age;
+            Speed = speed;
+        }
+        public void Jump()
+        {
+            Console.WriteLine("Гепард прыгает на {0} м", Length);
+        }
+        public void Run()
+        {
+            Console.WriteLine("Гепард бегает со скоростью {0} км/ч", Speed);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new()
+            {
+
+                new Cockroach(10, 12),
+                new Cockroach(32, 14),
+                new Cheetah(15, 42, 33),
+                new Cheetah(25, 30, 12),
+                new Kangaroo(14, 18),
+                new Kangaroo(28, 24)
+            };
+
+            animals.Sort();
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.GetType().Name}, возраст = {animal.Age}");
+            }
+
+            List<Kangaroo> kangaroos = new List<Kangaroo>();
+            foreach (var animal in animals)
+            {
+                if (animal is Kangaroo)
+                {
+                    kangaroos.Add(animal as Kangaroo);
+                }
+            }
+
+            kangaroos.Sort(new KangarooComparer());
+            foreach (var kangaroo in kangaroos)
+            {
+                Console.WriteLine($"Кенгуру, возраст = {kangaroo.Age}, длина = {kangaroo.Length}");
+            }
+
+
+            List<Cockroach> cockroaches = new List<Cockroach>();
+            foreach (var animal in animals)
+            {
+                if (animal is Cockroach)
+                {
+                    cockroaches.Add(animal as Cockroach);
+                }
+            }
+            cockroaches.Sort(new CockroachComparer());
+            foreach (var cockroach in cockroaches)
+            {
+                Console.WriteLine($"Таракан, возраст = {cockroach.Age}, скорость = {cockroach.Speed}");
+            }
+        }
+    }
+}

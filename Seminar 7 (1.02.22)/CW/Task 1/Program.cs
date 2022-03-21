4ef98c23cd4858166f7cf2d3b8b7c159f1565e4c
foxleren <foxleren@gmail.com>
2022-03-21 15:31:09 +0300

    add Seminar 7

---
+++ Seminar 7 (1.02.22)/CW/Task 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task_1
+{
+
+    class B : IComparable<B>
+    {
+        public int b;
+        public int CompareTo(B other)
+        {
+            return other.b - this.b;
+        }
+    }
+    struct A : IComparable<A>
+    {
+        public int a;
+        public int CompareTo(A other)
+        {
+            return other.a - this.a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100000;
+            A[] a = new A[n];
+            B[] b = new B[n];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int c = random.Next(1000);
+                a[i].a = c;
+                b[i] = new B();
+                b[i].b = c;
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Array.Sort(a);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            stopwatch.Start();
+            Array.Sort(b);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Seminar 7 (1.02.22)/HW/Task 1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Task_1
+{
+    public struct Person : IComparable<Person>
+    {
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException();
+                }
+
+                _name = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException();
+                }
+
+                _lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                _age = value;
+            }
+        }
+
+        private string _name;
+        private string _lastName;
+        private int _age;
+
+
+        public Person(string name, string lastName, int age) : this()
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public int CompareTo(Person person)
+        {
+            if (Age > person.Age)
+            {
+                return 1;
+            }
+
+            if (Age == person.Age)
+            {
+                return 0;
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            return $"Имя: {Name} | Фамилия: {LastName} | Возраст: {Age}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            string[] name = { "Jack", "Den", "Carl" };
+            string[] lastname = { "Black", "Red", "White" };
+            int n = int.Parse(Console.ReadLine());
+
+            Person[] persons = new Person[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                persons[i] = new Person(name[random.Next(name.Length)], lastname[random.Next(lastname.Length)], random.Next(1, 101));
+            }
+            Array.ForEach(persons, person => Console.WriteLine(person));
+
+            Array.Sort(persons);
+            Array.ForEach(persons, person => Console.WriteLine(person));
+        }
+    }
+}

480b0ac1c7e9f8fe1d56e683844312cf2b93cc6e
foxleren <foxleren@gmail.com>
2022-03-21 14:03:35 +0300

    add Homework

---
+++ Seminar 5 (25.01.22)/HW/HW/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HW
+{
+    public delegate double calculate(double x);
+
+    interface IFunction
+    {
+        double Function(double x);
+    }
+
+    class F : IFunction
+    {
+        calculate calculator;
+        public F(calculate a) => calculator = a;
+        public double Function(double a)
+        {
+            return calculator(a);
+        }
+    }
+
+    class G
+    {
+        F firstFunc;
+        F secondFunc;
+        public G(F f1, F f2)
+        {
+            firstFunc = f1;
+            secondFunc = f2;
+        }
+
+        public double GF(double x)
+        {
+            return firstFunc.Function(secondFunc.Function(x));
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            G g = new G(new F(x => x * x + 4), new F(x => Math.Sin(x)));
+            for (double i = 0; i < Math.PI; i += Math.PI / 16)
+            {
+                Console.WriteLine(g.GF(i));
+            }
+        }
+    }
+}

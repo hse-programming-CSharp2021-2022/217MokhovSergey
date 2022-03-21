bad9df8a0beca9c1c32fc34747d77a04faf09095
foxleren <foxleren@gmail.com>
2022-03-21 14:08:16 +0300

    add Homework 5

--- Seminar 5 (25.01.22)/HW/Task 1/obj/Debug/net5.0/Task 1.AssemblyInfo.cs
+++ Seminar 5 (25.01.22)/HW/Task 1/obj/Debug/net5.0/Task 1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HW")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HW")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HW")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task 1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.

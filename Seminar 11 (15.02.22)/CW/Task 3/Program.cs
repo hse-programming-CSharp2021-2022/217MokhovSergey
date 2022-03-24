09d3d8d7986a5a4adc22235c79e33eca4bd425b0
foxleren <foxleren@gmail.com>
2022-03-24 16:19:43 +0300

    update seminar 11

---
+++ Seminar 11 (15.02.22)/CW/Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter binaryWriter = new BinaryWriter(
+                       new FileStream("1.txt", FileMode.OpenOrCreate)))
+            {
+                binaryWriter.Write("123");
+            }
+
+            using (BinaryReader binaryReader = new BinaryReader(new FileStream("1.txt", FileMode.Open)))
+            {
+                Console.WriteLine(binaryReader.ReadString());
+            }
+        }
+    }
+}

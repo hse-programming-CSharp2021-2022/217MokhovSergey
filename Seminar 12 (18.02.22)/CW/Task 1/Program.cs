99db6ea4e0cac98f375ebc631ca2d05c9265a97d
foxleren <foxleren@gmail.com>
2022-03-24 17:33:51 +0300

    update seminar 12

---
+++ Seminar 12 (18.02.22)/CW/Task 1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Task_1
+{
+    [Serializable]
+    public class Person
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonIgnore]
+        public int Year { get; set; }
+
+        [NonSerialized]
+        private int _someValue;
+
+        [JsonInclude]
+        public int a = 100;
+        public int b = 200;
+
+        public Person(string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+        public Person() { }
+    }
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person("Bob", 1980);
+            Person person2 = new Person("Tom", 1995);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fileStream = new FileStream("output.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fileStream, person1);
+            }
+
+            using (FileStream fileStream = new FileStream("output.txt", FileMode.OpenOrCreate))
+            {
+                Person p = (Person)formatter.Deserialize(fileStream);
+                Console.WriteLine(p.Name + " " + p.Year);
+            }
+
+            Person[] people = new Person[] { person1, person2 };
+
+            using (FileStream fileStream = new FileStream("output.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fileStream, people);
+            }
+
+            using (FileStream fileStream = new FileStream("output.txt", FileMode.OpenOrCreate))
+            {
+                Person[] array = (Person[])formatter.Deserialize(fileStream);
+                foreach (var person in people)
+                {
+                    Console.WriteLine(person.Name + " " + person.Year);
+                }
+            }
+
+            // Xml не сериализует приватные поля и требует конструктор без параметров.
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+            using (FileStream fileStream = new FileStream("output1.txt", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fileStream, person1);
+            }
+
+            using (FileStream fileStream = new FileStream("output1.txt", FileMode.OpenOrCreate))
+            {
+                Person p = (Person)xmlSerializer.Deserialize(fileStream);
+                Console.WriteLine(p.Name + " " + p.Year);
+            }
+
+            XmlSerializer xmlSerializer2 = new XmlSerializer(typeof(Person[]));
+            using (FileStream fileStream = new FileStream("output4.txt", FileMode.OpenOrCreate))
+            {
+                xmlSerializer2.Serialize(fileStream, people);
+            }
+
+            using (FileStream fileStream = new FileStream("output4.txt", FileMode.OpenOrCreate))
+            {
+                Person[] array = (Person[])xmlSerializer2.Deserialize(fileStream);
+                foreach (var person in people)
+                {
+                    Console.WriteLine(person.Name + " " + person.Year);
+                }
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                WriteIndented = true
+            };
+
+            // По умолчанию сериализуются только свойства. Для полей пишем [JsonInclude]
+            string json = JsonSerializer.Serialize<Person>(person1, options);
+            Console.WriteLine(json);
+
+            Person person3 = JsonSerializer.Deserialize<Person>(json, options);
+            Console.WriteLine(person3.Name + " " + person3.Year);
+        }
+    }
+}
---
+++ Seminar 12 (18.02.22)/CW/Task 2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Task_2
+{
+    [Serializable]
+    public class Student
+    {
+        public string Surname { get; set; }
+        public int Course { get; set; }
+
+        public Student(string surname, int course)
+        {
+            Surname = surname;
+            Course = course;
+        }
+
+        public Student(){}
+    }
+
+    [Serializable]
+    public class Group
+    {
+        public int Id { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Group(int id, List<Student> students)
+        {
+            Id = id;
+            Students = students;
+        }
+
+        public Group(){}
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group1 = new Group(1, new List<Student>() { new Student("Black", 1), new Student("White", 1) });
+            Group group2 = new Group(2, new List<Student>() { new Student("Red", 2), new Student("Orange", 2) });
+
+            Group[] groups = new[] {group1, group2};
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fileStream = new FileStream("binary.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fileStream, groups);
+            }
+
+            using (FileStream fileStream = new FileStream("binary.txt", FileMode.Open))
+            {
+                var array = (Group[])formatter.Deserialize(fileStream);
+                foreach (var item in array)
+                {
+                    Console.WriteLine(item.Id);
+                }
+            }
+
+            // Все элементы сериализации должны быть Serializable.
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Group[]));
+            using (FileStream fileStream = new FileStream("xml.txt", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(fileStream, groups);
+            }
+
+            using (FileStream fileStream = new FileStream("xml.txt", FileMode.Open))
+            {
+                var array = (Group[])serializer.Deserialize(fileStream);
+                foreach (var item in array)
+                {
+                    Console.WriteLine(item.Id);
+                }
+            }
+
+
+            string json = JsonSerializer.Serialize(groups);
+
+            var array2 = JsonSerializer.Deserialize<Group[]>(json);
+            foreach (var item in array2)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}

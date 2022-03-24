cf0e04868ecd602ab0a0849dd4e3f6759ad321f9
foxleren <foxleren@gmail.com>
2022-03-24 22:18:32 +0300

    upload classwork for seminar 15

---
+++ Seminar 15 (1.03.22)/CW/Task 1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread current = Thread.CurrentThread;
+            //current.Name = "Name";
+            //Thread.Sleep(500);
+            //Console.WriteLine(current.Name);
+            //Thread.Sleep(500);
+            //Console.WriteLine(current.IsBackground);
+            //Thread.Sleep(500);
+            //Console.WriteLine(current.IsAlive);
+            //Thread.Sleep(500);
+            //Console.WriteLine(current.ManagedThreadId);
+            //Thread.Sleep(500);
+            //Console.WriteLine(current.Priority);
+            //Thread.Sleep(500);
+            //Console.WriteLine(current.ThreadState);
+            //Thread t1 = new Thread(() => Console.WriteLine(1));
+            //t1.Priority = ThreadPriority.Highest;
+            //Thread t2 = new Thread(() => Console.WriteLine(2));
+            //Thread t3 = new Thread(() => Console.WriteLine(3));
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+            //Console.WriteLine("end");
+
+            //Thread t4 = new Thread((a) => Console.WriteLine(a));
+            //t4.Priority = ThreadPriority.Highest;
+            //Thread t5 = new Thread((a) => Console.WriteLine(a));
+            //Thread t6 = new Thread((a) => Console.WriteLine(a));
+            //t4.Start(4);
+            //t5.Start(5);
+            //t6.Start(6);
+            //Console.WriteLine("end");
+
+            //int x = 0;
+            //object locker = new object();
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Thread thread = new Thread(Print);
+            //    thread.Start();
+            //}
+            //void Print()
+            //{
+            //    lock (locker)
+            //    {
+            //        for (int i = 0; i < 6; i++)
+            //        {
+            //            x++;
+            //            Thread.Sleep(100);
+            //            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {x}");
+            //        }
+            //    }
+            //}
+            //int x = 0;
+            //object locker = new object();
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Thread thread = new Thread(Print);
+            //    thread.Start();
+            //}
+            //void Print()
+            //{
+            //    bool l = false;
+            //    Monitor.Enter(locker, ref l);
+            //    for (int i = 0; i < 6; i++)
+            //        {
+            //            x++;
+            //            Thread.Sleep(100);
+            //            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {x}");
+            //        }
+            //    if (l)
+            //        Monitor.Exit(locker);
+            //}
+            //int x = 0;
+            //Mutex m = new Mutex();
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Thread thread = new Thread(Print);
+            //    thread.Start();
+            //}
+            //void Print()
+            //{
+            //    m.WaitOne();
+            //    for (int i = 0; i < 6; i++)
+            //    {
+            //        x++;
+            //        Thread.Sleep(100);
+            //        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {x}");
+            //    }
+            //    m.ReleaseMutex();
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                Visitor visitor = new Visitor(i);
+            }
+
+        }
+        class Visitor
+        {
+            static Semaphore sem = new Semaphore(3, 3);
+            Thread thread;
+            int count = 3;
+            public Visitor(int i)
+            {
+                thread = new Thread(Eat);
+                thread.Start();
+            }
+            public void Eat()
+            {
+                while (count > 0)
+                {
+                    sem.WaitOne();
+                    Console.WriteLine(count);
+                    Console.WriteLine($"{thread.ManagedThreadId} +");
+                    Console.WriteLine($"{thread.ManagedThreadId} is eating");
+                    Console.WriteLine($"{thread.ManagedThreadId} -");
+                    Thread.Sleep(100);
+                    sem.Release();
+                    count--;
+                }
+            }
+        }
+    }
+}
---
+++ Seminar 15 (1.03.22)/CW/Task 2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_2
+{
+    class BankAccount
+    {
+        private object thisLock = new object();
+
+        volatile int accountAmount;
+
+        Random random = new Random();
+
+        public BankAccount(int sum)
+        {
+            accountAmount = sum;
+        }
+
+        int Buy(int sum)
+        {
+            if (accountAmount == 0)
+            {
+                throw new InvalidOperationException($"Нулевой баланс...");
+            }
+
+            // условие никогда не выполнится, пока вы не закомментируете lock.  
+            if (accountAmount < 0)
+            {
+                throw new InvalidOperationException($"Отрицательный баланс");
+            }
+
+            //lock (thisLock)
+            //{
+            Monitor.Enter(thisLock);
+            if (accountAmount >= sum)
+            {
+                Console.WriteLine($"Состояние счета: {accountAmount}");
+                Console.WriteLine($" Покупка на сумму: {sum}");
+                accountAmount = accountAmount - sum;
+                Console.WriteLine($" Счет после пок.: {accountAmount}");
+                Monitor.Exit(thisLock);
+                return sum;
+            }
+            else
+            {
+                Monitor.Exit(thisLock);
+                return 0; // не хватает денег - отказываем в покупке
+            }
+            //}
+        }
+
+        public void DoTransactions()
+        {
+            try
+            {
+                while (true)
+                {
+                    Buy(random.Next(1, 50));
+                    Thread.Sleep(random.Next(1, 10));
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Обработано исключение: {ex.Message}. Поток завершает работу...");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Thread[] threads = new Thread[10];
+            BankAccount dep = new BankAccount(1000);
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(dep.DoTransactions);
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+        }
+
+    }
+}

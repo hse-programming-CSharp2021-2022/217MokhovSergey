87870c301913e62eedbd9aff19bd41f88ca28feb
foxleren <foxleren@gmail.com>
2022-03-24 19:05:30 +0300

    upload classwork for seminar 13

---
+++ Seminar 13 (22.02.22)/CW/Task 1/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    class Week : IEnumerable
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday" };
+        public IEnumerator GetEnumerator()
+        {
+            return days.GetEnumerator();
+        }
+    }
+    class Week2 : IEnumerable
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday" };
+        public IEnumerator GetEnumerator()
+        {
+            return new WeekEnumerator(days);
+        }
+        class WeekEnumerator : IEnumerator<string>
+        {
+            int position = -1;
+            string[] days;
+            public WeekEnumerator(string[] days)
+            {
+                this.days = days;
+            }
+            public string Current => days[position];
+            object IEnumerator.Current => days[position];
+            public void Dispose() { }
+            public bool MoveNext()
+            {
+                if (position < days.Length - 1)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+    }
+
+    class ArProgression : IEnumerable
+    {
+        int a0, d;
+        public ArProgression(int a0, int d)
+        {
+            this.a0 = a0; this.d = d;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new ArProgressionEnumerator(a0, d);
+        }
+        public IEnumerable NextElement(int n)
+        {
+            return new ArProgressionEnumerator(a0, d, n);
+        }
+        public IEnumerable NextElementYield(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                yield return a0 + d * i;
+            }
+        }
+        class ArProgressionEnumerator : IEnumerator, IEnumerable
+        {
+            int a0, d;
+            public ArProgressionEnumerator(int a0, int d)
+            {
+                this.a0 = a0; this.d = d;
+            }
+            public ArProgressionEnumerator(int a0, int d, int n)
+            {
+                this.a0 = a0; this.d = d; this.n = n;
+            }
+            int position = -1;
+            int n = 10;
+            public object Current => a0 + d * position;
+            public bool MoveNext()
+            {
+                if (position < n - 1)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public void Reset()
+            {
+                position = -1;
+            }
+            public IEnumerator GetEnumerator()
+            {
+                return this;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArProgression arProgression = new ArProgression(2, 4);
+            foreach (var a in arProgression)
+            {
+                Console.WriteLine(a);
+            }
+            foreach (var a in arProgression.NextElement(5))
+            {
+                Console.WriteLine(a);
+            }
+            foreach (var a in arProgression.NextElementYield(15))
+            {
+                Console.WriteLine(a);
+            }
+            //Week2 week = new Week2();
+            //foreach(var day in week)
+            //{
+            //    Console.WriteLine(day);
+            //}
+            //string[] str = { "freg", "fewfer", "3wfe" };
+            //foreach (string s in str)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //IEnumerator strenum = str.GetEnumerator();
+            //while(strenum.MoveNext())
+            //{
+            //    Console.WriteLine(strenum.Current);
+            //}
+        }
+    }
+}
---
+++ Seminar 13 (22.02.22)/CW/Task 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Task_2
+{
+
+    public class Fibbonacci
+    {
+        private int _el1 = 1;
+        private int _el2 = 1;
+        public IEnumerable GetElement(int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                yield return _el1;
+                int box = _el1 + _el2;
+                _el1 = _el2;
+                _el2 = box;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibbonacci myFibbonacci = new Fibbonacci();
+            foreach (var item in myFibbonacci.GetElement(5))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
